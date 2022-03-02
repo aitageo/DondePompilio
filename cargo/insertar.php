cfc91ac92dff75644351970522a12ad2b24c8346
aleja108 <aleja108@hotmail.com>
2022-03-01 21:12:53 -0500

    vista cargo

---
+++ cargo/insertar.php
@@ -0,0 +1,29 @@
+<?php
+
+include("conexion.php");
+
+if(isset($_POST['nuevo'])){
+
+
+$id_cargo = $_POST['id_cargo'];
+$nombre_cargo= $_POST['nombre_cargo'];
+$email = $_POST['mail'];
+$password = $_POST['password'];
+$cifrado = hash('sha512',$password);
+
+try{
+$query = "INSERT INTO  cargo VALUES($id_cargo,'$nombre_cargo','$email','$cifrado')";
+
+$result = $conexion->prepare($query);
+$result->execute();
+
+} catch(Exception $e){
+die("error".$e->getMessage());
+echo"ha ocurrido un error" .$e->getLine();
+}
+
+
+}
+
+
+?>
